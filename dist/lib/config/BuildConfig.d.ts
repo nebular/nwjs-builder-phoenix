809e52ab66be6efc58e004e4538dd38fc3cd2bef
rlp <rlp@nebular.tv>
2021-06-09 00:42:13 +0200

    dist

---
+++ dist/lib/config/BuildConfig.d.ts
@@ -0,0 +1,24 @@
+import { WinConfig } from './WinConfig';
+import { MacConfig } from './MacConfig';
+import { LinuxConfig } from './LinuxConfig';
+import { NsisConfig } from './NsisConfig';
+export declare class BuildConfig {
+    nwVersion: string;
+    nwFlavor: string;
+    output: string;
+    outputPattern: string;
+    packed: boolean;
+    targets: string[];
+    files: string[];
+    excludes: string[];
+    win: WinConfig;
+    mac: MacConfig;
+    linux: LinuxConfig;
+    nsis: NsisConfig;
+    prepackHook: string;
+    appId: string;
+    ffmpegIntegration: boolean;
+    strippedProperties: string[];
+    overriddenProperties: any;
+    constructor(pkg?: any);
+}
